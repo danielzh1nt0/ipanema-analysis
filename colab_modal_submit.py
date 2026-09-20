207d0183faedc6e462e9007b3f971fa00cb26774
Ipanema pipeline <ipanema@aurora-nordic.se>
2026-09-20 15:44:19 +0000

    modal submit: correct R2 key

--- colab_modal_submit.py
+++ colab_modal_submit.py
@@ -6,7 +6,7 @@ os.environ["MODAL_TOKEN_ID"] = userdata.get("MODAL_TOKEN_ID"); os.environ["MODAL
 subprocess.run("rm -rf /content/ipanema-analysis && git clone -q https://github.com/danielzh1nt0/ipanema-analysis.git /content/ipanema-analysis", shell=True)
 r = subprocess.run("cd /content/ipanema-analysis && modal deploy modal_app.py", shell=True, capture_output=True, text=True); print((r.stdout or r.stderr)[-300:])
 import modal
-MATCH = os.environ.get("IPANEMA_MATCH", "SFKBP1109_s1200"); VIDEO_URL = os.environ.get("IPANEMA_VIDEO_URL", f"{userdata.get('R2_PUBLIC_URL')}/{MATCH}/{MATCH}.mp4")
+MATCH = os.environ.get("IPANEMA_MATCH", "SFKBP1109_s1200"); VIDEO_URL = os.environ.get("IPANEMA_VIDEO_URL", f"{userdata.get('R2_PUBLIC_URL')}/{MATCH}/video.mp4")
 f = modal.Function.from_name("ipanema", "run_match")
 call = f.spawn(MATCH, VIDEO_URL)
 print("submitted", MATCH, "call id", call.object_id)
